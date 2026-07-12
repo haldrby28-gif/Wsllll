910ac9301da34e8c5a99fc2c8e0083ede7de5164
haldrby28-gif <haldrby28@gmail.com>
2026-07-12 05:03:17 +0300

    Update app_colors.dart

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,1 +1,13 @@
+// lib/core/constants/app_colors.dart
+import 'package:flutter/material.dart';
 
+class AppColors {
+  static const Color primaryOrange = Color(0xFFFF7A00);
+  static const Color darkOrange = Color(0xFFE06600);
+  static const Color lightOrange = Color(0xFFFFF0E0);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color textDark = Color(0xFF2B2B2B);
+  static const Color textGrey = Color(0xFF8A8A8A);
+  static const Color success = Color(0xFF2ECC71);
+  static const Color error = Color(0xFFE74C3C);
+}
